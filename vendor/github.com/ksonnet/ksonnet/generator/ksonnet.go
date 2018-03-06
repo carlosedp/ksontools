2b7e2fbbc3654d251b9ab6541d0368e0f4a10778
bryanl <bryanliles@gmail.com>
2018-03-06 08:30:09 -0500

    upgrading to ksonnet 0.9.0

---
+++ vendor/github.com/ksonnet/ksonnet/generator/ksonnet.go
@@ -0,0 +1,101 @@
+package generator
+
+import (
+	"encoding/json"
+	"io/ioutil"
+	"os"
+	"strings"
+
+	"github.com/blang/semver"
+	"github.com/ksonnet/ksonnet-lib/ksonnet-gen/kubespec"
+
+	"github.com/ksonnet/ksonnet-lib/ksonnet-gen/ksonnet"
+)
+
+var (
+	// ksonnetEmitter is the function which emits the ksonnet standard library.
+	ksonnetEmitter = ksonnet.GenerateLib
+	// revision is the current revision of ksonnet based on the git ref.
+	revision string
+)
+
+// KsonnetLib is the ksonnet standard library for a version of swagger.
+type KsonnetLib struct {
+	// K is ksonnet extensions.
+	K []byte
+	// K is the generated ksonnet library.
+	K8s []byte
+	// Swagger is the swagger JSON used to generate the library.
+	Swagger []byte
+	// Version is the API version of the swagger.
+	Version string
+}
+
+// Ksonnet generates the ksonnet standard library or returns an error if there was
+// a problem.
+func Ksonnet(swaggerData []byte) (*KsonnetLib, error) {
+	f, err := ioutil.TempFile("", "")
+	if err != nil {
+		return nil, err
+	}
+
+	defer os.Remove(f.Name())
+
+	var apiSpec kubespec.APISpec
+	if err = json.Unmarshal(swaggerData, &apiSpec); err != nil {
+		return nil, err
+	}
+
+	ver, err := semver.Make(strings.TrimPrefix(apiSpec.Info.Version, "v"))
+	if err != nil {
+		return nil, err
+	}
+
+	_, err = f.Write(swaggerData)
+	if err != nil {
+		return nil, err
+	}
+
+	if err = f.Close(); err != nil {
+		return nil, err
+	}
+
+	astMin := semver.MustParse("1.8.0")
+	if ver.LT(astMin) {
+		return textBuilder(&apiSpec, swaggerData)
+	}
+
+	return astBuilder(f.Name(), swaggerData)
+}
+
+func astBuilder(input string, swaggerData []byte) (*KsonnetLib, error) {
+	lib, err := ksonnetEmitter(input)
+	if err != nil {
+		return nil, err
+	}
+
+	kl := &KsonnetLib{
+		K:       lib.Extensions,
+		K8s:     lib.K8s,
+		Swagger: swaggerData,
+		Version: lib.Version,
+	}
+
+	return kl, nil
+}
+
+func textBuilder(apiSpec *kubespec.APISpec, swaggerData []byte) (*KsonnetLib, error) {
+	bK, bK8s, err := ksonnet.Emit(apiSpec, &revision, &revision)
+	if err != nil {
+		return nil, err
+	}
+
+	kl := &KsonnetLib{
+		K:       bK,
+		K8s:     bK8s,
+		Swagger: swaggerData,
+		Version: apiSpec.Info.Version,
+	}
+
+	return kl, nil
+}
